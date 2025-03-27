132272989115d3d1cf2af50f2bfcd7067ab959b1
m01x <fortiz.informatica@gmail.com>
2025-03-27 09:29:00 -0300

    feat( singleton ): implementado

--- 01-creacionales/06-singleton.ts
+++ 01-creacionales/06-singleton.ts
@@ -9,3 +9,74 @@
  *
  * https://refactoring.guru/es/design-patterns/singleton
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+
+class DragonBalls {
+
+    private static instance: DragonBalls;
+    private ballsCollected: number;
+
+    /**
+     * Constructor privado, porque solo se debe llamar desde dentro de la clase.
+     * Entonces como hacemos una instancia?? Se consulta si ya existe UNA instancia
+     * sino existe, la crea.
+     */
+
+    private constructor(){
+        this.ballsCollected = 0;
+    }
+
+    public static getInstance(): DragonBalls {
+        if( !DragonBalls.instance ){
+            DragonBalls.instance = new DragonBalls();
+            console.log('%cLas pelotas del dragón han sido creadas.', COLORS.green);
+        }
+        return DragonBalls.instance;
+    }
+
+    collectBall():void {
+        if( this.ballsCollected < 7){
+            this.ballsCollected++;
+            console.log(`Pelota recolectada. Total de pelotas: ${this.ballsCollected}`);
+            return;
+        }
+
+        console.log(`Ya se han recolectado las 7 esferas del dragon! Invoca a shenlong 🐲`);
+    }
+
+    summonShenLong(){
+        if (this.ballsCollected === 7){
+            console.log(`\nShenlong ha sido invocado, pide tu deseo!`);
+            this.ballsCollected = 0;
+            return;
+        }
+        console.log(`\nOh.. todavia no. Aun faltan ${7 - this.ballsCollected } pelotas.`)
+    }
+
+}
+
+function main(){
+
+    const gokuDragonBalls = DragonBalls.getInstance();
+
+    //usage..
+    gokuDragonBalls.collectBall();
+    gokuDragonBalls.collectBall();
+    gokuDragonBalls.collectBall();
+    gokuDragonBalls.collectBall();
+
+    gokuDragonBalls.summonShenLong();
+
+    const vegetaDragonBalls = DragonBalls.getInstance();
+    vegetaDragonBalls.collectBall();
+    vegetaDragonBalls.collectBall();
+    vegetaDragonBalls.collectBall();
+
+    gokuDragonBalls.summonShenLong();
+
+    vegetaDragonBalls.summonShenLong();
+}
+
+main();
