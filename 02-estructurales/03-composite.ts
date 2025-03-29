ae3c049d2639044d5463378907adb01a6c144958
m01x <fortiz.informatica@gmail.com>
2025-03-29 13:23:33 -0300

    feat( composite): implementado

--- 02-estructurales/03-composite.ts
+++ 02-estructurales/03-composite.ts
@@ -13,3 +13,67 @@
  * https://refactoring.guru/es/design-patterns/composite
  *
  */
+
+interface FileSystemComponent {
+    showDetails( indent?: string):void;
+}
+
+class File implements FileSystemComponent {
+    private name: string;
+
+    constructor( name:string ){
+        this.name = name;
+    }
+
+    showDetails(indent?: string): void {
+      console.log(`${ indent }- Archivo: ${this.name}`); //Que el archivo pueda mostrarse solo.
+    }
+}
+
+class Folder implements FileSystemComponent{
+    private name: string;
+    private contents: FileSystemComponent[] = [];
+
+    constructor( name: string){
+        this.name = name;
+    }
+
+    add( component: FileSystemComponent) {
+        this.contents.push( component );
+    }
+
+    showDetails(indent: string =''): void {
+      console.log(`${ indent }+ Carpeta: ${ this.name }`);
+      this.contents.forEach(component => component.showDetails( indent + ' '));
+    }
+}
+
+function main(){
+
+    const file1 = new File('Archivo1.txt');
+    const file2 = new File('Archivo2.txt');
+    const file3 = new File('Archivo3.txt');
+    const file4 = new File('Archivo4.txt');
+
+    const folder1 = new Folder('Carpeta 1');
+    const folder5 = new Folder('Carpeta 5');
+
+    folder1.add( file1 );
+    folder1.add( file2 );
+
+    const folder2 = new Folder('Carpeta 2');
+    folder2.add( file3 );
+    folder2.add( folder5 );
+
+    const folder3 = new Folder('Folder 3');
+    folder3.add(file4);
+    folder2.add(folder3);
+
+    const rootFolder = new Folder('Carpeta ROOT');
+    rootFolder.add(folder1);
+    rootFolder.add(folder2);
+
+    rootFolder.showDetails();
+}
+
+main();
