839afa8f9efc3ce9561547bbc5c610ae484e96fa
m01x <fortiz.informatica@gmail.com>
2025-04-03 14:50:22 -0300

    feat( facade ): TODO: Terminar Facade.

--- 02-estructurales/05-facade.ts
+++ 02-estructurales/05-facade.ts
@@ -11,3 +11,52 @@
  *
  * https://refactoring.guru/es/design-patterns/facade
  */
+
+class Projector {
+
+    turnOn(){
+        console.log(`Proyector encendido`;)
+    }
+    turnOff(){
+        console.log(`Proyector apagado`);
+    }
+}
+
+class SoundSystem {
+    on(){
+        console.log(`Sistema de sonido encendido`);
+    }
+
+    off(){
+        console.log(`Sistema de sonido apagado`);
+    }
+}
+
+class VideoPlayer {
+    on(){
+        console.log(`Video player encendido`);
+    }
+
+    play(movie: string){
+        console.log(`Reproduciendo %c`);
+    }
+
+    stop(){
+        console.log(`Pelicula detenida`);
+    }
+
+    off(){
+        console.log(`Video player apagado`);
+    }
+
+}
+
+class PopcornMaker {
+    poppingPopcorn(){
+        console.log(`Haciendo palomitas`);
+    }
+
+    turningOffPoppingPopcorn(){
+        //TODO implementar metodo... clase a medias!
+    }
+}
