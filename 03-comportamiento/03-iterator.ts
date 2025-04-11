2c83cedf6211e40fbac0eb103f1db8376ba4c621
m01x <fortiz.informatica@gmail.com>
2025-04-11 15:00:11 -0400

    feat( iterator ): TODO implementación iterator

--- 03-comportamiento/03-iterator.ts
+++ 03-comportamiento/03-iterator.ts
@@ -8,3 +8,56 @@
  *
  * https://refactoring.guru/es/design-patterns/iterator
  */
+
+
+interface Iterator<T> {
+    next():T | null;
+    hasNext(): boolean;
+    current(): T | null;
+}
+
+class Pokemon {
+
+    constructor( public name:string, public type:string){}
+
+   
+}
+
+class PokemonCollection {
+
+    private pokemons: Pokemon[] = [];
+
+    addPokemon( pokemon: Pokemon){
+        this.pokemons.push( pokemon);
+    }
+
+    getPokemonAt( index: number): Pokemon | null {
+        if( index >= 0 && index < this.pokemons.length){
+            return this.pokemons[index]
+        }
+
+        return null;
+    }
+    getLength(): number{
+        return this.pokemons.length;
+    }
+
+    //TODO
+    createIterator(){
+        console.log("Not implemented")
+    }
+}
+
+
+class PokemonIterator implements Iterator<Pokemon>{
+  next(): Pokemon | null {
+    throw new Error("Method not implemented.");
+  }
+  hasNext(): boolean {
+    throw new Error("Method not implemented.");
+  }
+  current(): Pokemon | null {
+    throw new Error("Method not implemented.");
+  }
+
+}
